50d335854dae7a4790b6c626de38d7cc62517d29
aprincer <gaprincipec@unc.edu.pe>
2021-08-27 12:36:30 -0500

    config

---
+++ app/src/main/java/com/appsereno/config/ConfigFtp.java
@@ -0,0 +1,17 @@
+package com.appsereno.config;
+
+public final class ConfigFtp {
+
+    public static String LOCAL_SERVER="127.0.0.1";
+    public static String LOCAL_USER="user";
+    public static String LOCAL_PASSWORD="password";
+    public static String LOCAL_DIRECTORY="dir";
+
+
+    public static String SERVER="ip_server";
+    public static String USER="usuario";
+    public static String PASSWORD="password";
+    public static String DIRECTORY="";
+
+    public static boolean isLocal=false;
+}
